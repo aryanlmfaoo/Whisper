d93cba48474a8a50ab7f52990c11b470faa825b2
Aryan Sharma <aryansharm1223@gmail.com>
2025-10-11 21:18:53 +0530

    Added DB Schema and signup route

--- backend/auth/src/index.ts
+++ backend/auth/src/index.ts
@@ -5,12 +5,16 @@ const app = express();
 const PORT = Number(process.env.PORT);
 
 if (!PORT) {
-    console.log("Port isnt defined in env variables.");
-    process.exit(1);
+  console.log("Port isnt defined in env variables.");
+  process.exit(1);
 }
+
+import signup from "./routes/unproteted/signup";
 
 app.use(express.json());
 
+app.use("/signup", signup);
+
 app.listen(PORT, () => {
-    console.log(`Connected to port ${PORT}`);
+  console.log(`Connected to port ${PORT}`);
 });
---
+++ backend/auth/src/routes/unproteted/signup.ts
@@ -0,0 +1,61 @@
+import { Router } from "express";
+import prisma from "../../prisma";
+import { genSalt, hash } from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const router = Router();
+
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+if (!JWT_SECRET) {
+  console.log("JWT Key not set");
+  process.exit(1);
+}
+
+router.post("/", async (req, res) => {
+  const { username, password, bio, name } = req.body;
+
+  if (!username || !password || !bio || !name) {
+    console.log("Invalid Input.");
+    return res.status(400).json({ success: false, message: "Invalid Input." });
+  }
+
+  try {
+    const salt = await genSalt(10);
+    const hashedPass = await hash(password, salt);
+
+    const user = await prisma.user.create({
+      username,
+      bio,
+      name,
+      password: hashedPass,
+    });
+
+    const token = jwt.sign({ user: user.id, username }, JWT_SECRET, {
+      expiresIn: "3hr",
+    });
+
+    /// for other service to create a user instance if required.
+    // const creatingInstances = Promise.all([])
+
+    return res.status(201).json({ success: true, token });
+  } catch (error) {
+    console.error(error);
+
+    await prisma.user
+      .delete({
+        where: {
+          username,
+        },
+      })
+      .then(() => {
+        console.log("Deleted user to ensure acid transaction.");
+      });
+
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server Error." });
+  }
+});
+
+export default router;
